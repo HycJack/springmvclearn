d83cc4f0a351d850f1b29d2d29143579de971d63
HycJack <772403255@qq.com>
2019-08-02 15:50:40 +0800

    [ADD] ADD server monitor,use oshi with JNA

---
+++ server/src/main/java/com/hycjack/server/controller/ServerController.java
@@ -0,0 +1,26 @@
+package com.hycjack.server.controller;
+
+import com.hycjack.server.domain.Server;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * 服务器监控
+ *
+ * @author ruoyi
+ */
+@Controller
+@RequestMapping("/monitor/server")
+public class ServerController {
+    private String prefix = "monitor/server";
+
+    @GetMapping()
+    public String server(ModelMap mmap) throws Exception {
+        Server server = new Server();
+        server.copyTo();
+        mmap.put("server", server);
+        return "/server";
+    }
+}
